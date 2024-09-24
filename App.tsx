3c83a6acf647168e195f09ff705fd43c0deb587d
seriojaa <119433399+sergiojaa@users.noreply.github.com>
2024-09-24 12:36:00 +0400

    Add files via upload

--- App.tsx
+++ App.tsx
@@ -7,24 +7,32 @@ import Child from './Child'
 // exla userefar kargavs tavis mnishvnelobas ar resetdeba
 //saitze tu gvinda rom ar gamochndes mashin vikenebt ogond.
 //nebismier mnishvnelobas heinaxavs string objcts arrays kvela elements
+// kvelaze xshirad inputebi inaxeba.
+//exla gavaketebt inputshi chaweril rames davakonsolebt
+
 function App() {
  const [show,setShow] = useState(false)
  const counter = useRef(0)
- console.log(counter)
+ const inputRef = useRef< HTMLInputElement >(null)
 
   return (
     <>
     <button onClick={()=>setShow(!show)} >show</button>
    {show ? <Child/> : null}
     
-    <button onClick={()=>{
+    <button 
+
+     onClick={()=>{
     counter.current++
     console.log(counter)
+    console.log(inputRef.current?.value) // chvenmier    chaweili value gamoprinta
+    console.log(inputRef.current?.classList) //klasi gamoprinta, tuaraak ar gamoprintavs.
     }
       
     }>click me </button>
   
-    
+    <input type="text" ref={inputRef} className='srg'
+     />
     </>
   )
 }
